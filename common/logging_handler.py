fdedb38b4e18c0b0e76f6d9ff5bab3cde1d4bd12
george <2578288992@qq.com>
2020-08-30 18:01:15 +0800

    home operation

--- common/logging_handler.py
+++ common/logging_handler.py
@@ -7,10 +7,11 @@ import logging.config
 import os
 
 root_dir = os.path.dirname(os.path.dirname(__file__))
+print(root_dir)
+# config_path = os.path.join(root_dir, 'config', 'log.conf')
 config_path = '/'.join((root_dir, 'config', 'log.conf'))
 
 # 实例化 logging
 CON_LOG = config_path
 logging.config.fileConfig(CON_LOG)
 logger = logging.getLogger()
-
